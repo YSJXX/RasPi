3d068edf3f55ed7965b88ca4eea6958758ac6982
RasPi_YSJ <424tjdwns@gmail.com>
2022-02-15 18:57:05 +0900

    v1.2 802.11 header file 생성
    1. uint48{}
    - Mac Addresses save
    
    2. _Radiotap{}
    - Radiotap header
    
    3. _ieee80211_beacon_frame{}
    - Beacon Frame header
    
    4. _tag_ssid{}
    - ssid header

--- src/Jpcap_Group.h
+++ src/Jpcap_Group.h
@@ -1,6 +1,33 @@
 #include <pcap/pcap.h>
 #define True 1
-#define Fales 0
+#define False 0
+
+//Mac Addresses save
+struct uint48{
+    unsigned long long v:48;
+}__attribute__((packed));
+
+typedef struct _Radiotap{
+    uint8_t j_revision;
+    uint8_t j_pad;
+    uint16_t j_length;
+    uint32_t j_present;
+}Radiotap;
+
+typedef struct _ieee80211_beacon_frame{
+    uint16_t j_Frame_control;
+    uint16_t j_Duration;
+    uint48 j_Destination_address;
+    uint48 j_Source_address;
+    uint48 j_BSSID;
+    uint16_t j_SequenceControl;
+}Ieee80211_beacon_frame;
+
+typedef struct _tag_ssid{
+    uint8_t j_tag_number;
+    uint8_t j_tag_length;
+    char SSID[32];
+}Tag_ssid;
 
 // Pcap Open Func
 pcap_t* OpenPcap(int select);
